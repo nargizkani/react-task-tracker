e91af0e5a685def010ed7a463d886d852211da65
nargiz <49775124+kaniyeva@users.noreply.github.com>
2021-10-20 14:16:38 +0600

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright &copy; 2021</p>
+            <Link to='/about'>About</Link>
+        </footer>
+    )
+}
+
+export default Footer
